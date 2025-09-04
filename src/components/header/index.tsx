5a9c076f7e5d9ba5c42d6fe84db11936855be5ab
gbmouraa <gabrielmoura-cma@hotmail.com>
2025-09-03 21:35:36 -0300

    first commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import { LiaGamepadSolid } from "react-icons/lia";
+
+export function Header() {
+  return (
+    <header className="w-full h-28 bg-slate-100 text-black px-2">
+      <div className="max-w-screen-xl mx-auto flex justify-center items-center h-28 sm:justify-between">
+        <nav className="flex justify-center items-center gap-4">
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="Logo do site DalyGames"
+              width={100}
+              height={150}
+              quality={100}
+              priority
+            />
+          </Link>
+          <Link href="/">Games</Link>
+          <Link href="/profile">Perfil</Link>
+        </nav>
+        <div className="hidden sm:flex justify-center items-center">
+          <Link href="/profile">
+            <LiaGamepadSolid size={34} color="#475569" />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
